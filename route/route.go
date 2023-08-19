2f1b6c7fd9e963e182afdf1923191a55feb49329
seungryeol <seungryeol156@gmail.com>
2023-08-19 15:58:39 +0900

    route 파일 추가

---
+++ route/route.go
@@ -0,0 +1,45 @@
+package route
+
+import (
+	"chat.com/p2p"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func Start() {
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.HandleFunc("/ws", socketHandler)
+
+	port := 8080
+	log.Printf("Listening on port %d", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func socketHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[socketHandler]")
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("upgrader.Upgrade: %v", err)
+		return
+	}
+
+	key := strconv.Itoa(len(p2p.Peers.V))
+
+	p := &p2p.Peer{
+		Conn: conn,
+		Key:  key,
+	}
+	p2p.Peers.V[key] = p
+
+	go p.Read()
+}
