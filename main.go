21948190e94359c0225b7fcdfa8c2c1d0478354c
seungryeol <seungryeol156@gmail.com>
2023-08-21 12:08:10 +0900

    router 결합

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"chat.com/rest"
 	"chat.com/route"
+	"chat.com/utils"
+	"github.com/gorilla/securecookie"
+	"os"
 )
 
 func main() {
 	port := 8080
 
+	setSessionKey()
+
+	// router가 두개 면 안된다
+	// 아래 코드의 라우터 병합이 필요
 	route.Start(port)
-	rest.Start(port)
+}
+
+func setSessionKey() {
+	var sessionKey = securecookie.GenerateRandomKey(32)
+	utils.HandleErr(os.Setenv("SESSION_KEY", string(sessionKey)))
 }
--- route/route.go
+++ route/route.go
@@ -2,26 +2,40 @@ package route
 
 import (
 	"chat.com/p2p"
+	"chat.com/utils"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 )
 
 func Start(port int) {
-	http.Handle("/", http.FileServer(http.Dir("static")))
-	http.HandleFunc("/ws", socketHandler)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/getUser", GetUserHandler).Methods("GET")
+	router.HandleFunc("/setUser", SetUserHandler).Methods("POST")
+	router.HandleFunc("/ws", socketHandler).Methods("POST")
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 
 	log.Printf("Listening on port %d", port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+}
+
+var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+
+type userRequestBody struct {
+	Name string
 }
 
 func socketHandler(w http.ResponseWriter, r *http.Request) {
@@ -41,4 +55,23 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 	p2p.Peers.V[key] = p
 
 	go p.Read()
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GetUserHandler")
+
+	userSession, _ := store.Get(r, "user")
+	json.NewEncoder(w).Encode(userSession.Values)
+}
+
+func SetUserHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "user")
+
+	var userRequestBody userRequestBody
+	json.NewDecoder(r.Body).Decode(&userRequestBody)
+
+	session.Values["user"] = userRequestBody
+
+	utils.HandleErr(session.Save(r, w))
+	w.WriteHeader(http.StatusCreated)
 }
