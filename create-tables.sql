c91ebe12e66bfcb9b32a23b7615c90104e946973
seungryeol <seungryeol156@gmail.com>
2023-09-29 23:34:28 +0900

    db connection test

---
+++ create-tables.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS room;
+DROP TABLE IF EXISTS chat;
+
+CREATE TABLE user (
+  id           INT AUTO_INCREMENT NOT NULL,
+  name         VARCHAR(128) NOT NULL,
+  password     VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY unique_name (`name`)
+);
+
+CREATE TABLE room (
+  id          INT AUTO_INCREMENT NOT NULL,
+  user_id     VARCHAR(128) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE chat (
+  id          INT AUTO_INCREMENT NOT NULL,
+  user_id     VARCHAR(128) NOT NULL,
+  room_id	  VARCHAR(128) NOT NULL,
+  content     VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
--- main.go
+++ main.go
@@ -2,9 +2,40 @@ package main
 
 import (
 	"chat.com/route"
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"log"
+	"os"
 )
 
+var db *sql.DB
+
 func main() {
+
+	fmt.Println("USER : ", os.Getenv("DBUSER"))
+	fmt.Println("PASSWORD : ", os.Getenv("DBPASS"))
+
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
 	port := 8080
 	route.Start(port)
 }
